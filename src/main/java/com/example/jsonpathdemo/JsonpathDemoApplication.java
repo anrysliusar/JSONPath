08ee8b82f5757c83027bf8c7f463b6f39e2f1b97
Andrew <assliusarenko@gmail.com>
2021-05-28 01:28:03 +0300

    Initial commit

---
+++ src/main/java/com/example/jsonpathdemo/JsonpathDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonpathdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpathDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonpathDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsonpathdemo/JsonpathDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonpathdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonpathDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
